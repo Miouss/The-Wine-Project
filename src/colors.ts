9f4b64d52bf31609b495eb4ba175a2d26fc9a3f4
Miouss <samir.ghabi@gmail.com>
2023-11-07 23:02:44 +0100

    fix custom colors for tailwind

--- src/colors.ts
+++ src/colors.ts
@@ -1,3 +1,5 @@
 export const black = "#1E1E1E";
 export const tertiary = "#B67561";
-export const whiteWine = "F8F6DF";
+export const wineWhite = "#F8F6DF";
+export const wineRed = "#FAEEEB";
+export const winePink = "#FAEBEE";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'tailwindcss'
-import { black, tertiary } from "./src/colors";
+import type { Config } from "tailwindcss";
+import { black, tertiary, winePink, wineRed, wineWhite } from "./src/colors";
 
 const config: Config = {
   content: [
@@ -8,24 +8,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black,
+      white: "#FFFFFF",
+    },
     extend: {
+      colors: {
+        tertiary,
+        "wine-white": wineWhite,
+        "wine-pink": winePink,
+        "wine-red": wineRed,
+      },
       backgroundImage: {
         footer: "url('/assets/footer/bg.png')",
-      },
-      backgroundColor: {
-        black,
-        tertiary,
-        "wine-white": "#F8F6DF",
-        "wine-pink": "#FAEBEE",
-        "wine-red": "#FAEEEB",
-      },
-      textColor: {
-        black,
-        tertiary,
-      },
-      gradientColorStops: {
-        black,
-        tertiary,
       },
       fontFamily: {
         cinzel: "var(--cinzel-font)",
