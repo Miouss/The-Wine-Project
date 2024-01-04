02a18766937fec5f6e844981dab38b780bf19d9b
Miouss <samir.ghabi@gmail.com>
2024-01-04 23:47:04 +0100

    fix footer

--- src/layouts/Mentions/images.tsx
+++ src/layouts/Mentions/images.tsx
@@ -1,13 +1,13 @@
 import { IMAGES_LAYOUTS_FOLDER } from "@/config";
 import Image from "next/image";
 
-export const Distinctions = () => {
+export const Distinctions = ({w = 10, h = 10}) => {
   const nbDistinctions = 4;
 
   return (
     <ol className="h-full w-full flex gap-4">
       {Array.from({ length: nbDistinctions }, (_, i) => (
-        <div key={i} className="relative w-10 h-10">
+        <div key={i} className={`relative w-${w} h-${h}`}>
           <Image
             src={`${IMAGES_LAYOUTS_FOLDER}/Mentions/distinction${i}.svg`}
             fill={true}
--- src/layouts/Mentions/index.tsx
+++ src/layouts/Mentions/index.tsx
@@ -2,9 +2,9 @@ import { Distinctions } from "./images";
 
 export function Mentions() {
   return (
-    <div className="flex flex-col lg:flex-col gap-4">
-      <h4 className="text-xl">Nos Distinctions</h4>
-      <Distinctions />
+    <div className="flex flex-col lg:flex-col gap-4 max-w-[336px] w-[100%]">
+      <h4 className="text-2xl">Nos Distinctions</h4>
+      <Distinctions w={20} h={20} />
     </div>
   );
 }
